c78c8d9a12e6b1c0c2eaff012c460059b4785494
liyang1221 <19198926@qq.com>
2014-10-20 17:17:35 +0800

    -update project

---
+++ Source/iDoodle/DoodleBoard.xaml.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Shapes;
+
+namespace iDoodle
+{
+    public partial class DoodleBoard : UserControl
+    {
+        PathFigure fig;
+        PathGeometry geo;
+        Path path;
+
+        bool m_isTouch = false;
+
+        Stack<UIElement> StepStack = new Stack<UIElement>();
+
+        public DoodleBoard()
+        {
+            InitializeComponent();
+            //path.FillRule = FillRule.Nonzero;
+            this.PenWidth = 5;
+            this.PenFill = new SolidColorBrush(Colors.Black);
+            this.PenStroke = new SolidColorBrush(Colors.Black);
+            this.PenOpacity = 1.0;
+        }
+
+        private void LayoutRoot_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (m_isTouch)
+            {
+                LineSegment seg = new LineSegment();
+                seg.Point = e.GetPosition(LayoutRoot);
+
+                fig.Segments.Add(seg);
+                path.Data = geo;
+
+                if (!LayoutRoot.Children.Contains(path))
+                {
+                    LayoutRoot.Children.Add(path);
+                }
+            }
+            //System.Diagnostics.Debug.WriteLine("MouseMove");
+        }
+
+        private void LayoutRoot_MouseDown(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            fig = new PathFigure();
+            fig.IsClosed = false;
+            fig.IsFilled = false;
+            fig.StartPoint = e.GetPosition(LayoutRoot);
+            geo = new PathGeometry();
+            geo.FillRule = FillRule.Nonzero;
+            geo.Figures.Add(fig);
+            path = new Path();
+            path.Data = geo;
+            path.Fill = this.PenFill;
+            path.Stroke = this.PenStroke;
+            path.Opacity = this.PenOpacity;
+            path.StrokeThickness = this.PenWidth;
+            path.StrokeEndLineCap = PenLineCap.Round;
+            path.StrokeStartLineCap = PenLineCap.Round;
+            this.m_isTouch = true;
+        }
+
+        private void LayoutRoot_MouseUp(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (path != null && !LayoutRoot.Children.Contains(path))
+            {
+                EllipseGeometry ell = new EllipseGeometry();
+                
+                ell.Center = e.GetPosition(LayoutRoot);
+                ell.RadiusX = 0.1;
+                ell.RadiusY = 0.1;
+
+                var point = new Path();
+                point.Data = ell;
+                point.Fill = this.PenFill;
+                point.Stroke = this.PenStroke;
+                point.StrokeThickness = this.PenWidth;
+                LayoutRoot.Children.Add(point);
+            }
+            this.m_isTouch = false;
+        }
+
+        private void LayoutRoot_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if(m_isTouch) LayoutRoot_MouseDown(this, e);
+        }
+
+        private void LayoutRoot_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            
+        }
+
+        #region public interface
+
+        /// <summary>
+        /// 生成画图板的一副实时图，可用于预览或保存
+        /// </summary>
+        /// <returns></returns>
+        public WriteableBitmap GetImage(bool big = false)
+        {
+            if (!big)
+            {
+                var scale = new ScaleTransform();
+                scale.ScaleX = 0.5;
+                scale.ScaleY = 0.5;
+                return new WriteableBitmap(LayoutRoot, scale);
+            }
+            return new WriteableBitmap(LayoutRoot, null);
+        }
+
+        /// <summary>
+        /// 撤销画图板的最后一次操作
+        /// </summary>
+        public void StepBack()
+        {
+            if (this.CanStepBack)
+            {
+                StepStack.Push(LayoutRoot.Children.Last());
+                LayoutRoot.Children.RemoveAt(LayoutRoot.Children.Count - 1);
+                //PathRoot.Data = path;
+            }
+        }
+
+        /// <summary>
+        /// 确定是否可以撤销操作
+        /// </summary>
+        public bool CanStepBack
+        {
+            get
+            {
+                return LayoutRoot.Children.Count > 0;
+            }
+        }
+
+        /// <summary>
+        /// 重做画图板的最近一次撤销
+        /// </summary>
+        public void StepForward()
+        {
+            if (CanStepForward)
+            {
+                LayoutRoot.Children.Add(StepStack.Pop());
+                //PathRoot.Data = path;
+            }
+        }
+
+        /// <summary>
+        /// 确定是否可以重做撤销操作
+        /// </summary>
+        public bool CanStepForward
+        {
+            get
+            {
+                return StepStack.Count > 0;
+            }
+        }
+
+        /// <summary>
+        /// 清除画图板上所有的内容
+        /// </summary>
+        public void Clear()
+        {
+            LayoutRoot.Children.Clear();
+            //PathRoot.Data = path;
+        }
+
+        /// <summary>
+        /// 获取或设置画笔的宽度
+        /// </summary>
+        public double PenWidth { get; set; }
+
+        public Brush PenFill { get; set; }
+
+        public Brush PenStroke { get; set; }
+
+        public Brush BoardBrush
+        {
+            get { return LayoutRoot.Background; }
+            set
+            {
+                LayoutRoot.Background = value;
+            }
+        }
+
+        public double PenOpacity { get; set; }
+
+        #endregion
+    }
+}
---
+++ Source/iDoodle/MainPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using iDoodle.Resources;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+
+namespace iDoodle
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // 构造函数
+        public MainPage()
+        {
+            InitializeComponent();
+
+            board.PenWidth = 10;
+        }
+
+        private void Btn_Save_Click(object sender, EventArgs e)
+        {
+            var bmp = board.GetImage(true);
+
+            var lib = new Microsoft.Xna.Framework.Media.MediaLibrary();
+
+            using(var stream = new System.IO.MemoryStream())
+            {
+                System.Windows.Media.Imaging.Extensions.SaveJpeg(bmp,stream,bmp.PixelWidth,bmp.PixelHeight,0,100);
+                stream.Flush();
+                var name = "i_doodle_" + DateTime.Now.ToString("yyyyMMddHHmmss");
+
+                lib.SavePicture(name, stream.GetBuffer());
+            }
+        }
+
+        private void Btn_Back_Click(object sender, EventArgs e)
+        {
+            if (board.CanStepBack) board.StepBack();
+        }
+
+        private void Btn_Next_Click(object sender, EventArgs e)
+        {
+            if (board.CanStepForward) board.StepForward();
+        }
+
+        private void Btn_Clear_Click(object sender, EventArgs e)
+        {
+            board.Clear();
+        }
+
+        private void Btn_Refresh_Click(object sender, EventArgs e)
+        {
+            var bmp = board.GetImage();
+
+            previewWnd.Height = bmp.PixelHeight / 4;
+            previewWnd.Width = bmp.PixelWidth / 4;
+            previewWnd.Source = bmp;
+        }
+
+        private void Btn_PenWidth_Click(object sender, RoutedEventArgs e)
+        {
+            if (Slider_PW.Visibility == System.Windows.Visibility.Visible)
+                Slider_PW.Visibility = System.Windows.Visibility.Collapsed;
+            else
+                Slider_PW.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void Slider_PW_Changed(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (board != null) board.PenWidth = (e.NewValue - 1) * 4 + 1;
+        }
+
+        private void Slider_PC_Changed(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Color c = Colors.Black;
+            switch ((int)e.NewValue)
+            {
+                case 1: c = Colors.White; break;
+                case 2: c = Colors.Red; break;
+                case 3: c = Colors.Yellow; break;
+                case 4: c = Colors.Blue; break;
+                case 5: c = Colors.Green; break;
+                case 6: c = Colors.Black; break;
+            }
+            SolidColorBrush brush = new SolidColorBrush(c);
+            (sender as Slider).Foreground = brush;
+
+            if (board != null)
+            {
+                board.PenStroke = brush;
+            }
+        }
+
+        private void Btn_PenColor_Click(object sender, RoutedEventArgs e)
+        {
+            if (Slider_PC.Visibility == System.Windows.Visibility.Visible)
+                Slider_PC.Visibility = System.Windows.Visibility.Collapsed;
+            else
+                Slider_PC.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void Slider_PO_Changed(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var opa = e.NewValue * 0.16 + 0.04;
+            ((sender as Slider).Foreground) = new SolidColorBrush(Colors.Red) { Opacity = opa, };
+            if (board != null)
+            {
+                board.PenOpacity = opa;
+            }
+        }
+
+        private void Btn_PenOpacity_Click(object sender, RoutedEventArgs e)
+        {
+            if (Slider_PO.Visibility == System.Windows.Visibility.Visible)
+                Slider_PO.Visibility = System.Windows.Visibility.Collapsed;
+            else
+                Slider_PO.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void Btn_Open_Click(object sender, EventArgs e)
+        {
+            Microsoft.Phone.Tasks.PhotoChooserTask task = new Microsoft.Phone.Tasks.PhotoChooserTask();
+            task.ShowCamera = true;
+            task.Completed += (ss, ee) =>
+            {
+                if (ee.TaskResult == Microsoft.Phone.Tasks.TaskResult.OK)
+                {
+                    ImageBrush brush = new ImageBrush();
+                    BitmapImage img = new BitmapImage();
+                    img.SetSource(ee.ChosenPhoto);
+                    brush.ImageSource = img;
+                    board.BoardBrush = brush;
+                }
+            };
+            task.Show();
+        }
+
+        void SliderInAnim(Control control, AnimDirection dir, int to = 0, int time = 500)
+        {
+            ExponentialEase ease = new ExponentialEase();
+            ease.EasingMode = EasingMode.EaseOut;
+            ease.Exponent = 5;
+            
+        }
+    }
+
+    enum AnimDirection
+    {
+        Left,
+        Right,
+        Top,
+        Bottom,
+    }
+}
